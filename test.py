2f400b5cfe56ba1022e3f46de76d08b4bb13d13a
lucastroncoso <lucastroncoso.seguros@gmail.com>
2022-08-22 23:28:50 +0200

    python script fix

--- test.py
+++ test.py
@@ -3,9 +3,15 @@ from time import sleep
 from unittest import result
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from parsel import Selector
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+options = Options()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 try:
     driver.get('https://www.linkedin.com/')
     username = driver.find_element(By.CLASS_NAME, 'input__input')

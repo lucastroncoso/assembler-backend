c0b3ad6d9b0f4e9513cc0663a687f31b17db0a73
lucastroncoso <lucastroncoso.seguros@gmail.com>
2022-08-22 23:53:43 +0200

    python heroku test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from time import sleep
 from unittest import result
 from selenium import webdriver
@@ -9,9 +10,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from parsel import Selector
 
 # driver = webdriver.Chrome()
-options = Options()
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# options = Options()
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 try:
     driver.get('https://www.linkedin.com/')
     username = driver.find_element(By.CLASS_NAME, 'input__input')
